cb3bb30e4b3ee62c5c7f3e08eac8ced485698ac4
Dankleberg <75868313+Dankleberg@users.noreply.github.com>
2023-04-23 23:18:52 -0500

    Update imagedownloader.py

--- imagedownloader.py
+++ imagedownloader.py
@@ -1,22 +1,22 @@
 import requests
 import os
 
-linksfile = input('Enter path to file with links: ')
+linksfile = input(r'Enter path to file with links: ')
+linksfile = linksfile.strip('"');
+outputdirectory = input(r"Enter directory you would like to save images to: ")
+outputdirectory = outputdirectory.strip('"');
 links = []
 
 with open(linksfile, "r", encoding="utf8") as file:
     links = file.readlines()
     for link in links:
-        print(link.split()[1])
         print(os.path.basename(link.split()[1]) + "\n")
-        print(os.path.basename(link.split()[1])[-3:])
         if '.' in link.split()[1] and (os.path.basename(link.split()[1])[-3:] == "jpg" or
                                        os.path.basename(link.split()[1])[-3:] == "png" or
                                        os.path.basename(link.split()[1])[-3:] == "gif" or
-                                       os.path.basename(link.split()[1])[-3:] == "jpeg"):
-            with open(rf"C:\Users\ament\Downloads\RedditSaved\SavedPictures\{os.path.basename(link.split()[1])}",
+                                       os.path.basename(link.split()[1])[-4:] == "jpeg"):
+            with open(rf"{outputdirectory}\{os.path.basename(link.split()[1])}",
                       'wb') as handle:
-                print(True)
                 response = requests.get(link.split()[1], stream=True)
 
                 if not response.ok:
